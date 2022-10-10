869bd39a3ce0dd5577dfe50cdf8b6815ac55d2ba
zhouchaojiang <zhouchaojiang@ofworld.com>
2022-10-10 19:56:27 +0800

    Initial Commit

---
+++ INote/INoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  INoteApp.swift
+//  INote
+//
+//  Created by 周朝江 on 2022/10/10.
+//
+
+import SwiftUI
+
+@main
+struct INoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
